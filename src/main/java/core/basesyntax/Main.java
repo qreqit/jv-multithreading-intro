5b678d7766afc6d36524c3f9a6022db0bde03446
greqit <greqit@gmail.com>
2026-04-09 20:53:59 +0300

    Add implementation myThread by interface and Class

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Runnable myRunnable = new MyRunnable();
+        Thread thread1 = new MyThread();
+        Thread thread2 = new Thread(myRunnable);
+        thread2.setDaemon(true);
+
+        thread1.start();
+        thread2.start();
     }
 }
